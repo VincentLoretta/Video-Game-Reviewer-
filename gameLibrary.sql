a330d4c081731759cbedcae6546db8fae3aa2469
VincentLoretta <vloretta7413@gmail.com>
2023-05-08 20:41:11 -0400

    Added our SQL file

---
+++ gameLibrary.sql
@@ -0,0 +1,35 @@
+create database gameLibrary; 
+USE gameLibrary;
+CREATE table USER(
+Username varchar(18),
+pword varchar(18),
+email varchar(18),
+userid int(6),
+usertype varchar(18),
+primary key(userid)
+);
+
+create table Games(
+title varchar(255),
+genre varchar(255),
+publisher varchar(255),
+developer varchar(255),
+release_date date,
+rating int
+);
+
+create table Reviews(
+title varchar(255),
+Body varchar(255),
+Likes int,
+ReviewID int,
+userid int
+);
+
+
+
+
+
+
+
+
